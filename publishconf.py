8937ed2bd153bd0d14465a91f78d7a359de61136
rsip22 <rsip22@openmailbox.org>
2017-11-16 20:50:50 -0200

    Fix SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://rsip22.github.io/rsip22'
+SITEURL = 'https://rsip22.github.io/blog'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
