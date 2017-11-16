b049183dfb7cc54101dc3b894269863b9301a009
rsip22 <rsip22@openmailbox.org>
2017-11-16 17:50:58 -0200

    Makes the layout mobile-friendly

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,39 +6,51 @@ AUTHOR = 'Renata'
 SITENAME = "Renata's blog"
 SITEURL = ''
 
+THEME = 'themes/pelican-simplegrey'
+# BOOTSTRAP_THEME = 'simplegrey'
+
+HIDE_CATEGORIES_FROM_MENU = True
+DISPLAY_PAGES_ON_MENU = False
+
 PATH = 'content'
+WITH_FUTURE_DATES = False
 
 TIMEZONE = 'America/Sao_Paulo'
 
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-"""
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 FEED_ALL_RSS = 'feeds/all.rss.xml'
+# CATEGORY_FEED_ATOM = None
+# TRANSLATION_FEED_ATOM = None
+# AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
-"""
+
+PELICAN_SIMPLEGREY_TWITTER_CARD_ACCOUNT = 'rsip22'
+TWITTER_USERNAME = 'rsip22'
+OPEN_GRAPH_IMAGE = 'opengraph.png'
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Outreachy', 'http://outreachy.org/'),
+         ('PyLadies Porto Alegre', 'https://pyladiespoa.pythonanywhere.com'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Quitter', 'https://www.quitter.se/rsip22'),
+          ('Twitter', 'https://www.twitter.com/rsip22'),
+          ('Github', 'https://www.github.com/rsip22'),)
 
-DEFAULT_PAGINATION = 6
+DEFAULT_PAGINATION = 5
+
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+
+STATIC_PATHS = ['img']
+
 """
 FILES_TO_COPY = (
     ('extra/CNAME', 'CNAME'),
 )
 """
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = False
