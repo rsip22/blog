1bee01b063b120e424fb650b7c0c3704ef3f5ffe
Renata D'Avila <renatadavila@pm.me>
2020-08-17 06:39:58 -0300

    Add SITEURL to publishconf because of relative URLs

--- publishconf.py
+++ publishconf.py
@@ -11,9 +11,10 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = ''
+SITEURL = 'https://rsip22.github.io/blog/'
 RELATIVE_URLS = False
 
+FEED_DOMAIN = SITEURL
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
